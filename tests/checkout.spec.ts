7e5b1a6831fca089d16b6d5965b01192803999ac
Alvaro Deguer <alvarodeguer@gmail.com>
2026-02-13 03:08:53 -0300

    chore: Update test with multi-product checkout
    - Adjusted checkout test to handle multi-product checkout in a robust way.

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -6,7 +6,8 @@ import { CheckoutPage } from '../pages/checkout.page';
 import { USERS } from '../data/users';
 
 test.describe('Checkout Flow', () => {
-  test('should complete a purchase successfully', async ({ page }) => {
+
+  test('single product checkout', async ({ page }) => {
     const loginPage = new LoginPage(page);
     const inventoryPage = new InventoryPage(page);
     const cartPage = new CartPage(page);
@@ -33,4 +34,39 @@ test.describe('Checkout Flow', () => {
 
     await expect(checkoutPage.confirmationMessage).toHaveText(/Thank you for your order!/);
   });
+
+  test('multi-product checkout', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const inventoryPage = new InventoryPage(page);
+    const cartPage = new CartPage(page);
+    const checkoutPage = new CheckoutPage(page);
+
+    await loginPage.goto();
+    await loginPage.login(USERS.valid);
+
+    await inventoryPage.goto();
+
+    await inventoryPage.page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+    await inventoryPage.page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+    await inventoryPage.page.locator('[data-test="add-to-cart-sauce-labs-onesie"]').click();
+
+    await expect(inventoryPage.cartBadge).toHaveText('3');
+
+    await cartPage.goto();
+    await expect(cartPage.cartItems).toHaveCount(3);
+
+    await cartPage.clickCheckout();
+
+    await checkoutPage.fillInfo({
+      firstName: 'Alvaro',
+      lastName: 'Tester',
+      zipCode: '4000'
+    });
+    await checkoutPage.continue();
+
+    await checkoutPage.finish();
+
+    await expect(checkoutPage.confirmationMessage).toHaveText(/Thank you for your order!/);
+  });
+
 });
